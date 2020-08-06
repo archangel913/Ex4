3b8fb1feaec79b80238fdaaeb9fb795f8fc4bb79
横井　貴大 <yokoi@yokoitakashidainoMacBook-Air.local>
2020-08-06 20:47:02 +0900

    User name added

--- Heads or Tails.c
+++ Heads or Tails.c
@@ -3,7 +3,11 @@
 #include <time.h>
 int main (){
     int r,i,heads=0,tails=0;
+    char name[25];
     srand((unsigned int)time(NULL));
+    printf("Who are you?\n");
+    scanf("%s",name);
+    printf("Hello, %s!\n",name);
     printf("Tossing a coin...\n");
     for(i=1;i<4;i++){
         printf("Round %d: ",i);
